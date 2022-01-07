a06f6e6eb84d5921e6c68ef204707c37eca5806e
juwewong <juwewong@ucsc.edu>
2022-01-06 17:18:32 -0800

    Luca 1/06 - Part 2 progress before dinner break

--- Lab01/Lab1_part2.X/part2.c
+++ Lab01/Lab1_part2.X/part2.c
@@ -10,14 +10,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+int main(void) {
     BOARD_Init();
     /***************************************************************************************************
      * Your code goes in between this comment and the following one with asterisks.
      **************************************************************************************************/
 
+    // Declare Variables
+    float fahr, celsius;
+    int lower, upper, step;
+    char F, C;
 
+    // Initializing Variables 
+    lower = 0; // lower limit of temperature
+    upper = 300; // upper limit ""
+    step = 20; // step size
+    fahr = lower;
+
+    // Print out table
+    printf("%5c %5c\n", 'F', 'C');
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0)*(fahr - 32.0);
+        printf("%7.1f %04.0f\n", (double) fahr, (double) celsius);
+        fahr = fahr + step;
+    }
 
     /***************************************************************************************************
      * Your code goes in between this comment and the preceding one with asterisks.
