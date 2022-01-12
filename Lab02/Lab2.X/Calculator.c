b1b70fb7c8769fb900341b0dfb59b8eae7dd78f7
Luca Altaffer <taltaffe@ucsc.edu>
2022-01-11 19:09:54 -0800

    Luca 1/11 - Finished code and initial debug. Need to compile and test.

--- Lab02/Lab2.X/Calculator.c
+++ Lab02/Lab2.X/Calculator.c
@@ -15,27 +15,193 @@
 // **** Declare function prototypes ****
 double Add(double operand1, double operand2);
 double Multiply(double operand1, double operand2);
+double Divide(double operand1, double operand2);
+double Subtract(double operand1, double operand2);
+double Average(double operand1, double operand2);
 double AbsoluteValue(double operand);
 double FahrenheitToCelsius(double operand);
+double CelsiusToFahrenheit(double operand1);
+double Round(double operand1);
 double Tangent(double operand);
 //add more prototypes here
 
-
-void CalculatorRun(void)
-{
-    printf("\n\nWelcome to Luca's calculator program! Compiled at %s %s\n",__DATE__, __TIME__);
+void CalculatorRun(void) {
+    printf("\n\nWelcome to Luca's calculator program! Compiled at %s %s\n", __DATE__, __TIME__);
 
     // Your code here
     BOARD_End();
-    while (1);
+    while (1) {
+        // Initializing the operator as a char
+        char op[1];
+        printf("Enter a mathematical operation to perform (*,/,+.-.v.a.c.f.t.r): ");
+        scanf("%c", op);
+        if (op == "*") {
+            double operator1, operator2;
+
+            // Getting the first operand
+            printf("Enter the first operand: ");
+            scanf("%f", &operator1);
+
+            // Getting the second operand
+            printf("Enter the second operand: ");
+            scanf("%f", &operator2);
+
+            // performing tbe math
+            double answer = Multiply(operator1, operator2);
+
+            //print result
+            printf("Result of (", operator1, " * ", operator2, "): ", answer);
+
+        } else if (op == "/") {
+            double operator1, operator2;
+
+            // Getting the first operand
+            printf("Enter the first operand: ");
+            scanf("%f", &operator1);
+
+            // Getting the second operand
+            printf("Enter the second operand: ");
+            scanf("%f", &operator2);
+
+            // If the second operand is 0
+            if (operator2 == 0) {
+                printf("Error: Divide by 0!");
+            } else {
+
+                // performing tbe math
+                double answer = Divide(operator1, operator2);
+
+                //print result
+                printf("Result of (", operator1, " / ", operator2, "): ", answer);
+            }
+
+        } else if (op == "+") {
+            double operator1, operator2;
+
+            // Getting the first operand
+            printf("Enter the first operand: ");
+            scanf("%f", &operator1);
+
+            // Getting the second operand
+            printf("Enter the second operand: ");
+            scanf("%f", &operator2);
+
+            // performing tbe math
+            double answer = Add(operator1, operator2);
+
+            //print result
+            printf("Result of (", operator1, " + ", operator2, "): ", answer);
+
+        } else if (op == "-") {
+            double operator1, operator2;
+
+            // Getting the first operand
+            printf("Enter the first operand: ");
+            scanf("%f", &operator1);
+
+            // Getting the second operand
+            printf("Enter the second operand: ");
+            scanf("%f", &operator2);
+
+            // performing tbe math
+            double answer = Subtract(operator1, operator2);
+
+            //print result
+            printf("Result of (", operator1, " - ", operator2, "): ", answer);
+
+        } else if (op == "m") {
+            double operator1, operator2;
+
+            // Getting the first operand
+            printf("Enter the first operand: ");
+            scanf("%f", &operator1);
+
+            // Getting the second operand
+            printf("Enter the second operand: ");
+            scanf("%f", &operator2);
+
+            // performing tbe math
+            double answer = Average(operator1, operator2);
+
+            //print result
+            printf("Result of Averaging(", operator1, " & ", operator2, "): ", answer);
+
+        } else if (op == "a") {
+            double operator1;
+
+            // Getting the first operand
+            printf("Enter the first operand: ");
+            scanf("%f", &operator1);
+
+            // performing tbe math
+            double answer = AbsoluteValue(operator1);
+
+            //print result
+            printf("Result of |", operator1, "|: ", answer);
+
+        } else if (op == "c") {
+            double operator1;
+
+            // Getting the first operand
+            printf("Enter the first operand: ");
+            scanf("%f", &operator1);
+
+            // performing tbe math
+            double answer = FahrenheitToCelsius(operator1);
+
+            //print result
+            printf("Result of Fahrenheit", operator1, " to Celsius ", answer);
+
+        } else if (op == "f") {
+            double operator1;
+
+            // Getting the first operand
+            printf("Enter the first operand: ");
+            scanf("%f", &operator1);
+
+            // performing tbe math
+            double answer = CelsiusToFahrenheit(operator1);
+
+            //print result
+            printf("Result of Celsius", operator1, " to Fahrenheit ", answer);
+
+        } else if (op == "t") {
+            double operator1;
+
+            // Getting the first operand
+            printf("Enter the first operand: ");
+            scanf("%f", &operator1);
+
+            // performing tbe math
+            double answer = Tangent(operator1);
+
+            //print result
+            printf("Result of tan(", operator1, "): ", answer);
+
+        } else if (op == "q") {
+            // exits out
+            break;
+        } else if (op == "r") {
+            double operator1;
+
+            // Getting the first operand
+            printf("Enter the first operand: ");
+            scanf("%f", &operator1);
+
+            // performing tbe math
+            double answer = Round(operator1);
+
+            //print result
+            printf("Result of Rounding(", operator1, "): ", answer);
+
+        }
+    }
 }
-
-
 
 /********************************************************************************
  * Define the Add function here.
  ********************************************************************************/
-double Add(double operand1, double operand2){
+double Add(double operand1, double operand2) {
     // conducting addition between operands
     double result = (operand1 + operand2);
     // return the result
@@ -45,17 +211,16 @@ double Add(double operand1, double operand2){
 /********************************************************************************
  * Define the Subtract function here.
  ********************************************************************************/
-double Subtract(double operand1, double operand2)
-{
+double Subtract(double operand1, double operand2) {
     double result = (operand1 - operand2);
     return result;
-	
+
 }
 
 /********************************************************************************
  * Define the Multiply function here.
  ********************************************************************************/
-double Multiply(double operand1, double operand2){
+double Multiply(double operand1, double operand2) {
     double result = (operand1 * operand2);
     return result;
 }
@@ -63,17 +228,16 @@ double Multiply(double operand1, double operand2){
 /********************************************************************************
  * Define the Divide function here.
  ********************************************************************************/
-double Divide(double operand1, double operand2)
-{
+double Divide(double operand1, double operand2) {
     double result = (operand1 / operand2);
     return result;
-	
+
 }
 
 /********************************************************************************
  * Define the Absolute Value function here.
  ********************************************************************************/
-double Absolute(double operand1){
+double AbsoluteValue(double operand1) {
     double result = abs(operand1);
     return result;
 }
@@ -81,7 +245,7 @@ double Absolute(double operand1){
 /*********************************************************************************
  * Define the Fahrenheit to Celsius function here.
  ********************************************************************************/
-double F2C(double operand1){
+double FahrenheitToCelsius(double operand1) {
     double result = ((operand1 - 32) * (5 / 9));
     return result;
 }
@@ -89,7 +253,7 @@ double F2C(double operand1){
 /*********************************************************************************
  * Define the Celsius to Fahrenheit function here.
  ********************************************************************************/
-double C2F(double operand1){
+double CelsiusToFahrenheit(double operand1) {
     double result = ((operand1 + (9 / 5)) + 32);
     return result;
 }
@@ -97,7 +261,7 @@ double C2F(double operand1){
 /********************************************************************************
  * Define the Average function here.
  *******************************************************************************/
-double Average(double operand1, double operand2){
+double Average(double operand1, double operand2) {
     double result = ((operand1 + operand2) / 2);
     return result;
 }
@@ -105,8 +269,8 @@ double Average(double operand1, double operand2){
 /*********************************************************************************
  * Define the Tangent function that takes input in degrees.
  ********************************************************************************/
-double Tangent(double operand1){
-    double result = tan((operand1 * (180 / M_PI )));
+double Tangent(double operand1) {
+    double result = tan((operand1 * (180 / M_PI)));
     return result;
 }
 
@@ -115,8 +279,7 @@ double Tangent(double operand1){
  * In order to receive the extra credit your calculator MUST ALSO CORRECTLY utilize
  * this function.
  ********************************************************************************/
-double Round(double operand)
-{
+double Round(double operand) {
     double result = floor(operand);
     return result;
 }
