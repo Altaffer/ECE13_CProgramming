dc62d9b8d1064f5ed991d4abd678a8d31b9abe01
Luca Altaffer <taltaffe@ucsc.edu>
2022-01-20 16:37:17 -0800

    Luca 1/20 - Finished Tests script afetr following notes. Need to fix a few functions

--- Lab03/Lab3.X/mml_test.c
+++ Lab03/Lab3.X/mml_test.c
@@ -13,10 +13,31 @@
 
 int TestsPassed, PassedNumber;
 
+// HELPER FUNCTIONS
+int FloatEquals(float x, float y) {
+    if (x > (y + FP_DELTA) || x < (y - FP_DELTA)) {
+        return FALSE;
+    }
+    return TRUE;
+}
+
+int MatrixEquals2x2(float mat1[2][2], float mat2[2][2]) {
+    int row, column;
+    for (row = 0; row < 2; row++) {
+        for (column = 0; column < 2; column++) {
+            if (mat1[row][column]>(mat2[row][column] + FP_DELTA) ||
+                    mat1[row][column]<(mat2[row][column] - FP_DELTA)) {
+                return FALSE;
+            }
+        }
+    }
+    return TRUE;
+}
+
 int main()
 {
     BOARD_Init();
-    TestsPassed = 0
+    TestsPassed = 0;
             
     // Empty matrices for function results
     float result2[2][2] = {
@@ -236,6 +257,33 @@ int main()
         {2, -2},
         {0, 1}
     };
+    
+    // Scalars for functions 
+    float g1 = 1;
+    float g2 = 5;
+    float g3 = 10;
+    float g4 = 100;
+
+    // i and j values for submatrix tests
+    int i1 = 1;
+    int j1 = 2;
+    int i2 = 0;
+    int j2 = 1;
+    int i3 = 2;
+    int j3 = 0;
+
+
+    // Expected results for Determinant tests
+    float x1 = 24;
+    float x2 = 324;
+    float x3 = 10;
+    float x4 = 1;
+
+    // Expected results for Trace tests
+    float t1 = 82.5; //trace value for matrix5
+    float t2 = 0.165; //trace value for matrixH
+    float t3 = 5.297; //trace value for matrixL
+    float t4 = -15; //trace value for matrix
 
     printf("Beginning taltaffe's mml test harness, compiled on %s %s\n", __DATE__, __TIME__);
     
@@ -270,6 +318,246 @@ int main()
         printf("FAILED (%d/4) : MatrixEquals\n", PassedNumber);
     }
 
+    // TESTING MATRIX ADD
+    PassedNumber = 0;
+    // Test 1 
+    MatrixAdd(matrixA, matrixB, result3);
+    if (MatrixEquals(result3, matrix1)) PassedNumber++;
+    // Test 2
+    MatrixAdd(matrixD, matrixE, result3);
+    if (MatrixEquals(result3, matrix2)) PassedNumber++;
+    // Test 3
+    MatrixAdd(matrixA, matrixH, result3);
+    if (MatrixEquals(result3, matrix10)) PassedNumber++;
+    // Test 4 - Testing property A+B == B+A;
+    MatrixAdd(matrixE, matrixD, result3);
+    if (MatrixEquals(result3, matrix2)) PassedNumber++;
+
+
+    if (PassedNumber >= 4) {
+        printf("PASSED (%d/4) : MatrixAdd\n", PassedNumber);
+        TestsPassed++;
+    } else {
+        printf("FAILED (%d/4) : MatrixAdd\n", PassedNumber);
+    }
+
+    // TESTING MATRIX MULTIPLY
+    PassedNumber = 0;
+
+    // Test 1 
+    MatrixMultiply(matrixD, matrixE, result3);
+    if (MatrixEquals(result3, matrix11)) PassedNumber++;
+
+    // Test 2 
+    MatrixMultiply(matrixM, matrixN, result3);
+    if (MatrixEquals(result3, matrix12)) PassedNumber++;
+
+    // Test 3
+    MatrixMultiply(matrixD, matrixO, result3);
+    if (MatrixEquals(result3, matrix13)) PassedNumber++;
+
+    // Test 4 - Test to confirm that A*B != B*A
+    MatrixMultiply(matrixE, matrixD, result3);
+    if (!MatrixEquals(result3, matrix11)) PassedNumber++;
+
+    if (PassedNumber >= 4) {
+        printf("PASSED (%d/4) : MatrixMultiply\n", PassedNumber);
+        TestsPassed++;
+    } else {
+        printf("FAILED (%d/4) : MatrixMultiply\n", PassedNumber);
+    }
+    
+    // TESTING SCALR ADD
+    PassedNumber = 0;
+    // Test 1
+    MatrixScalarAdd(g1, matrixD, result3);
+    if (MatrixEquals(result3, matrix3)) PassedNumber++;
+
+    // Test 2
+    MatrixScalarAdd(g2, matrixF, result3);
+    if (!MatrixEquals(result3, matrixE)) PassedNumber++;
+
+    // Test 3
+    MatrixScalarAdd(g3, matrixF, result3);
+    if (MatrixEquals(result3, matrixE)) PassedNumber++;
+
+    // Test 4
+    MatrixScalarAdd(g4, matrixM, result3);
+    if (MatrixEquals(result3, matrix20)) PassedNumber++;
+
+
+    if (PassedNumber >= 3) {
+        printf("PASSED (%d/4) : MatrixScalarAdd\n", PassedNumber);
+        TestsPassed++;
+    } else {
+
+        printf("FAILED (%d/4) : MatrixScalarAdd\n", PassedNumber);
+    }
+
+    // TESTING SCALAR MULTIPLY
+    PassedNumber = 0;
+    // Test 1
+    MatrixScalarMultiply(g3, matrixF, result3);
+    if (MatrixEquals(result3, matrix4)) PassedNumber++;
+
+    // Test 2
+    MatrixScalarMultiply(g2, matrixG, result3);
+    if (MatrixEquals(result3, matrix5)) PassedNumber++;
+
+    // Test 3
+    MatrixScalarMultiply(g4, matrixH, result3);
+    if (MatrixEquals(result3, matrix6)) PassedNumber++;
+
+    // Test 4
+    MatrixScalarMultiply(g2, matrix4, result3);
+    if (MatrixEquals(result3, matrix21)) PassedNumber++;
+
+    if (PassedNumber >= 4) {
+        printf("PASSED (%d/4) : MatrixScalarMultiply\n", PassedNumber);
+        TestsPassed++;
+    } else {
+        printf("FAILED (%d/4) : MatrixScalarMultiply\n", PassedNumber);
+    }
+
+    // TESTING TRACE
+    PassedNumber = 0;
+    // Test 1
+    float t = 0;
+    t = MatrixTrace(matrix5);
+    if (FloatEquals(t, t1)) PassedNumber++;
+
+    // Test 2
+    float s = 0;
+    s = MatrixTrace(matrixH);
+    if (FloatEquals(s, t2)) PassedNumber++;
+
+    // Test 3
+    float r = 0;
+    r = MatrixTrace(matrixL);
+    if (FloatEquals(r, t3)) PassedNumber++;
+
+    // Test 4
+    float q = 0;
+    q = MatrixTrace(matrixF);
+    if (FloatEquals(q, t4)) PassedNumber++;
+
+
+    if (PassedNumber >= 4) {
+        printf("PASSED (%d/4) : MatrixTrace\n", PassedNumber);
+        TestsPassed++;
+    } else {
+        printf("FAILED (%d/4) : MatrixTrace\n", PassedNumber);
+    }
+    
+    
+    // TESTING TRANSPOSE
+    PassedNumber = 0;
+    // Test 1
+    MatrixTranspose(matrixD, result3);
+    if (MatrixEquals(result3, matrix7)) PassedNumber++;
+
+    // Test 2
+    MatrixTranspose(matrixG, result3);
+    if (MatrixEquals(result3, matrix16)) PassedNumber++;
+
+    // Test 3
+    MatrixTranspose(matrixJ, result3);
+    if (MatrixEquals(result3, matrix17)) PassedNumber++;
+    
+    // Test 4
+    MatrixTranspose(matrixF, result3);
+    if (MatrixEquals(result3, matrix22)) PassedNumber++;
+
+    if (PassedNumber >= 4) {
+        printf("PASSED (%d/4) : MatrixTranspose\n", PassedNumber);
+        TestsPassed++;
+    } else {
+        printf("FAILED (%d/4) : MatrixTranspose\n", PassedNumber);
+    }
+    
+    
+    // TESTING SUBMATRIX 
+    PassedNumber = 0;
+    // Test 1
+    MatrixSubmatrix(i1, j1, matrixD, result2);
+    if (MatrixEquals2x2(result2, matrix8)) PassedNumber++;
+
+    // Test 2
+    MatrixSubmatrix(i2, j2, matrixL, result2);
+    if (MatrixEquals2x2(result2, matrix18)) PassedNumber++;
+
+    // Test 3
+    MatrixSubmatrix(i3, j3, matrixH, result2);
+    if (MatrixEquals2x2(result2, matrix19)) PassedNumber++;
+    
+    // Test 4
+    MatrixSubmatrix(i2, j2, matrixQ, result2);
+    if (MatrixEquals2x2(result2, matrix23)) PassedNumber++;
+
+    if (PassedNumber >= 4) {
+        printf("PASSED (%d/4) : MatrixSubmatrix\n", PassedNumber);
+        TestsPassed++;
+    } else {
+        printf("FAILED (%d/4) : MatrixSubmatrix\n", PassedNumber);
+    }
+    
+    
+    // TESTING DETERMINANT
+    PassedNumber = 0;
+    // Test 1
+    int y = MatrixDeterminant(matrixI);
+    if (FloatEquals(x1, y)) PassedNumber++;
+    // Test 2
+    y = MatrixDeterminant(matrixJ);
+    if (FloatEquals(x2, y)) PassedNumber++;
+
+    // Test 3
+    y = MatrixDeterminant(matrixQ);
+    if (FloatEquals(x3, y)) PassedNumber++;
+    
+    // Test 4
+    y = MatrixDeterminant(matrixP);
+    if (FloatEquals(x4, y)) PassedNumber++;
+
+    if (PassedNumber >= 4) {
+        printf("PASSED (%d/4) : MatrixDeterminant\n", PassedNumber);
+        TestsPassed++;
+    } else {
+        printf("FAILED (%d/4) : MatrixDeterminant\n", PassedNumber);
+    }
+    
+    // TESTING INVERSE
+    PassedNumber = 0;
+    //Test 1
+    MatrixInverse(matrixK, result3);
+    if (MatrixEquals(result3, matrix9)) PassedNumber++;
+
+    // Test 2
+    MatrixInverse(matrixP, result3);
+    if (MatrixEquals(result3, matrix14)) PassedNumber++;
+
+    // Test 3
+    MatrixInverse(matrixQ, result3);
+    if (MatrixEquals(result3, matrix15)) PassedNumber++;
+
+    // Test 4
+    MatrixInverse(matrixR, result3);
+    if (MatrixEquals(result3, matrixR)) PassedNumber++;
+
+
+    if (PassedNumber >= 4) {
+        printf("PASSED (%d/4) : MatrixInverse\n", PassedNumber);
+        TestsPassed++;
+    } else {
+        printf("FAILED (%d/4) : MatrixInverse\n", PassedNumber);
+    }
+    
+    
+    // PRINTING RESULTS
+    printf("-------------------------------------\n");
+    printf("%d out of 10 functions passed (%.2f%%)\n", TestsPassed,
+            (double) ((TestsPassed * 100) / 10));
+    
     // Add more tests here!
     BOARD_End();
     while (1);
