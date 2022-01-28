57fa1ff1ccc4d93affa56a87071159682e361a85
Luca Altaffer <taltaffe@ucsc.edu>
2022-01-27 16:53:58 -0800

    Luca 1/27 - pushing before ping pong

--- Lab04/Lab4.X/stack.c
+++ Lab04/Lab4.X/stack.c
@@ -1,7 +1,40 @@
 #include "stack.h"
 
+// Creating function to inialize the Stack
 void StackInit(struct Stack *stack) {
     stack->currentItemIndex = -1; 
     stack->initialized = TRUE;
 }
 
+// Function to push a value onto teh stack
+int StackPush(struct Stack *stack, double value){
+    &stack->currentItemIndex += 1;
+    &stack->stackItems[stack->currentItemIndex] = value;
+}
+
+// function to remove element from stack
+int StackPop(struct Stack *stack, double *value){
+    &value = stack->stackItems[stack->currentItemIndex];
+    &stack->stackItems[stack->currentItemIndex] = NULL;
+    &stack->currentItemIndex -= 1;
+}
+
+// Checks whether stack is empy or not
+int StackIsEmpty(const struct Stack *stack){
+    if(&stack->currentItemIndex == -1 ){
+        return SUCCESS;
+    }
+    else{
+        return NO_SUCCESS
+    }
+}
+
+// Checks if Stack is full or not 
+int StackIsFull(const struct Stack *stack){
+    
+}
+
+// Getter for the zie of the stack
+int StackGetSize(const struct Stack *stack){
+    
+}
