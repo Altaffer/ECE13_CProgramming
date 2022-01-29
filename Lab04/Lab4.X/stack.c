6bbf803893f81f82ee0ce6cba8810d3dc097504e
Luca Altaffer <taltaffe@ucsc.edu>
2022-01-28 16:06:26 -0800

    Luca 1/28 - Tests script almost done for stack. Push before Spirited away

--- Lab04/Lab4.X/stack.c
+++ Lab04/Lab4.X/stack.c
@@ -1,40 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "BOARD.h"
 #include "stack.h"
 
 // Creating function to inialize the Stack
+
 void StackInit(struct Stack *stack) {
-    stack->currentItemIndex = -1; 
+    stack->currentItemIndex = -1;
     stack->initialized = TRUE;
 }
 
-// Function to push a value onto teh stack
-int StackPush(struct Stack *stack, double value){
-    &stack->currentItemIndex += 1;
-    &stack->stackItems[stack->currentItemIndex] = value;
+// Function to push a value onto the stack
+
+int StackPush(struct Stack *stack, double value) {
+    if (stack->currentItemIndex == 19) {
+        printf("The stack is full");
+        return STANDARD_ERROR;
+    } else {
+        stack->currentItemIndex += 1;
+        stack->stackItems[stack->currentItemIndex] = value;
+        return SUCCESS;
+    }
 }
 
 // function to remove element from stack
-int StackPop(struct Stack *stack, double *value){
-    &value = stack->stackItems[stack->currentItemIndex];
-    &stack->stackItems[stack->currentItemIndex] = NULL;
-    &stack->currentItemIndex -= 1;
-}
 
-// Checks whether stack is empy or not
-int StackIsEmpty(const struct Stack *stack){
-    if(&stack->currentItemIndex == -1 ){
+int StackPop(struct Stack *stack, double *value) {
+    if (stack->currentItemIndex == -1) {
+        printf("The stack is empty");
+        return STANDARD_ERROR;
+    } else {
+        value = &stack->stackItems[stack->currentItemIndex];
+        stack->stackItems[stack->currentItemIndex--];
         return SUCCESS;
     }
-    else{
-        return NO_SUCCESS
+}
+
+// Checks whether stack is empty or not
+
+int StackIsEmpty(const struct Stack *stack) {
+    if (stack->currentItemIndex == -1) {
+        return SUCCESS;
+    } else {
+        return STANDARD_ERROR;
     }
 }
 
 // Checks if Stack is full or not 
-int StackIsFull(const struct Stack *stack){
-    
+
+int StackIsFull(const struct Stack *stack) {
+ if (stack->currentItemIndex == 19) {
+        return SUCCESS;
+    } else {
+        return STANDARD_ERROR;
+    }
 }
 
-// Getter for the zie of the stack
-int StackGetSize(const struct Stack *stack){
-    
+// Getter for the size of the stack
+
+int StackGetSize(const struct Stack *stack) {
+    return (stack->currentItemIndex + 1); 
 }
--- Lab04/Lab4.X/stack_test.c
+++ Lab04/Lab4.X/stack_test.c
@@ -9,21 +9,109 @@
 // User libraries
 #include "stack.h"
 
-int main()
-{
+int testsPassed = 0;
+
+int main() {
     BOARD_Init();
 
-    printf("\n###### Beginning CRUZID's stack test harness: ####\n\n");
+    printf("\n###### Beginning taltaffe's stack test harness: ####\n\n");
 
     //What follows is starter code.  You will need to modify it!
+
+    void testStackInit() {
+        printf("Does StackInit() set the currentItemIndex appropriately?\n");
+        struct Stack stack = {};
+        StackInit(&stack);
+        if (stack.currentItemIndex == -1) {
+            printf("passed\n");
+            testsPassed++;
+        } else {
+            printf("failed\n");
+        }
+        printf("\nPassed %i/1 StackInit tests\n\n", testsPassed);
+        testsPassed = 0;
+    }
+
+    void testStackPush() {
+        printf("Does StackPush() push a value onto the stack?\n");
+        struct Stack stack = {1, 2, 3};
+        StackPush(&stack, 4);
+        printf("%i", stack.stackItems);
+        if (stack.stackItems[3] == 4) {
+            printf("passed\n");
+            testsPassed++;
+        } else {
+            printf("failed\n");
+        }
+        printf("Does StackPush() push a value when its full?\n");
+        struct Stack stack = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        StackPush(&stack, 21);
+        printf("%i", stack.stackItems);
+        if (StackPush(&stack, 21) == STANDARD_ERROR) {
+            printf("passed\n");
+            testsPassed++;
+        } else {
+            printf("failed\n");
+        }
+        printf("\nPassed %i/2 StackPush tests\n\n", testsPassed);
+        testsPassed = 0;
+    }
+
+    void testStackPop() {
+        printf("Does StackPop() pop a value onto the stack?\n");
+        struct Stack stack = {1, 2, 3};
+        StackPop(&stack, 1);
+        printf("%i", stack.stackItems);
+        if (stack.stackItems[0] == 2) {
+            printf("passed\n");
+            testsPassed++;
+        } else {
+            printf("failed\n");
+        }
+        printf("Does StackPush() pop when struct is empty?\n");
+        struct Stack stack = {};
+        StackPush(&stack, 1);
+        printf("%i", stack.stackItems);
+        if (StackPop(&stack, 1) == STANDARD_ERROR) {
+            printf("passed\n");
+            testsPassed++;
+        } else {
+            printf("failed\n");
+        }
+    printf("\nPassed %i/2 StackPop tests\n\n", testsPassed);
+    testsPassed = 0;
+    }
+
+    void testStackIsEmpty() {
+        printf("Does StackIsEmpty() identify an empty stack?\n");
+        struct Stack stack = {};
+        StackIsEmpty(&stack);
+        if (StackIsEmpty(&stack) == TRUE) {
+            printf("passed\n");
+            testsPassed++;
+        } else {
+            printf("failed\n");
+        }
+        printf("Does StackIsEmpty() identify false case?\n");
+        struct Stack stack = {1};
+        StackIsEmpty(&stack);
+        if (StackIsEmpty(&stack) == FALSE) {
+            printf("passed\n");
+            testsPassed++;
+        } else {
+            printf("failed\n");
+        }
+        
+        printf("\nPassed %i/2 StackIsEmpty tests\n\n", testsPassed);
+        testsPassed = 0;
+    }
     
-    printf("Does StackInit() set the currentItemIndex appropriately?\n");
-    struct Stack stack = {};
-    StackInit(&stack);
-    if (stack.currentItemIndex == -1) {
-        printf("passed");
-    } else {
-        printf("failed");
+    void testStackIsFull() {
+        
+    }
+    
+    void testGetStackSize(){
+        
     }
 
     BOARD_End();
