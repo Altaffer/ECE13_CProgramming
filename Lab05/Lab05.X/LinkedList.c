6531a65c2cd08f602ee9a412df2242d36540f3b5
Luca Altaffer <taltaffe@ucsc.edu>
2022-02-04 20:13:00 -0800

    Luca 2/4 - LinkedList progress. Need to finish swap data nad write test script.

--- Lab05/Lab05.X/LinkedList.c
+++ Lab05/Lab05.X/LinkedList.c
@@ -8,39 +8,110 @@
 // User libraries
 #include "LinkedList.h"
 
-ListItem *LinkedListNew(char *data){
-    
-}
-
-ListItem *LinkedListCreateAfter(ListItem *item, char *data){
-    if (ListItem->previousItem == NULL){
-        return;
+ListItem *LinkedListNew(char *data) {
+    struct ListItem *newItem = (struct ListItem*) malloc(sizeof (struct ListItem));
+    if (newItem == NULL) {
+        return NULL;
     }
-    
-    struct ListItem *newItem = (struct ListItem*) malloc(size of(struct ListItem));
-    newItem->data = &data;
+    newItem->data = data;
+    newItem->nextItem = NULL;
+    newItem->previousItem = NULL;
+    return newItem;
 }
 
-char *LinkedListRemove(ListItem *item){
-    
+ListItem *LinkedListCreateAfter(ListItem *item, char *data) {
+    LinkedListNew(&data);
+
+    struct ListItem *newItem = LinkedListNew(&data);
+    if (newItem == NULL) {
+        return NULL;
+    }
+    if (item == NULL) {
+        return newItem;
+    }
+    if (item->nextItem == NULL) {
+        item->nextItem = newItem;
+        newItem->previousItem = item;
+    } else {
+        item->nextItem->previousItem = newItem;
+        item->nextItem = newItem;
+        newItem->previousItem = item;
+    }
+    return newItem;
 }
 
-int LinkedListSize(ListItem *list){
-    
+char *LinkedListRemove(ListItem * item) {
+    if (item == NULL) {
+        return NULL;
+    }
+    char* dat = item->data;
+    if ((item->previousItem == NULL) || (item->nextItem == NULL)) {
+        free(item);
+    } else {
+        item->nextItem->previousItem = item->previousItem;
+        item->previousItem->nextItem = item->nextItem;
+        free(item);
+    }
+
+    return dat;
 }
 
-ListItem *LinkedListGetFirst(ListItem *list){
-    
+int LinkedListSize(ListItem * list) {
+    if (list = NULL){
+        return NULL;
+    }
+    struct ListItem *head = LinkedListGetFirst(list);
+    int count = 0;
+    while (head != NULL){
+        count++;
+        head = head->nextItem;
+    }
+    return count;
 }
 
-ListItem *LinkedListGetLast(ListItem *list){
-    
+ListItem * LinkedListGetFirst(ListItem * list) {
+    if (list == NULL) {
+        return NULL;
+    }
+    if (list->previousItem == NULL){
+        return list;
+    }else{
+        while(list->previousItem != NULL){
+            list = list->previousItem;
+        }
+    }
+    return list;
 }
 
-int LinkedListSwapData(ListItem *firstItem, ListItem *secondItem){
-    
+ListItem * LinkedListGetLast(ListItem * list) {
+    if (list == NULL){
+        return NULL;
+    }
+    if (list->nextItem = NULL){
+        return list;
+    }else{
+        while(list->nextItem != NULL){
+            list = list->nextItem;
+        }
+    }
+    return list;
+}   
+
+int LinkedListSwapData(ListItem *firstItem, ListItem * secondItem) {
+
 }
 
-int LinkedListPrint(ListItem *list){
-    
+int LinkedListPrint(ListItem * list) {
+    if(list == NULL){
+        return STANDARD_ERROR;
+    }
+    struct ListItem *head = LinkedListGetFirst(list);
+    int count = 0;
+    while (head != NULL){
+        count++;
+        printf("%d, ", head);
+        head = head->nextItem;
+    }
+    return SUCCESS;
+
 }
